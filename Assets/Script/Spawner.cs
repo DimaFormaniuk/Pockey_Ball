2bfb54073ad43445f12cedab8a19ae503b623507
DimaFormaniuk <44363635+DimaFormaniuk@users.noreply.github.com>
2021-02-04 19:18:05 +0200

    lesson 3

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] private Block _segmentTemplate;
+    [SerializeField] private Block _blockTemplate;
+    [SerializeField] private Block _finishTemplate;
+    [SerializeField] private int _towerSize;
 
+    private void Start()
+    {
+        BuildTower();
+    }
+
+    private void BuildTower()
+    {
+        GameObject currentPoint = gameObject;
+
+        for (int i = 0; i < _towerSize; i++)
+        {
+            currentPoint = BuildSegment(currentPoint, _segmentTemplate.gameObject);
+
+            currentPoint = BuildSegment(currentPoint, _blockTemplate.gameObject);
+        }
+
+        BuildSegment(currentPoint, _finishTemplate.gameObject);
+    }
+
+    private GameObject BuildSegment(GameObject currentSegment,GameObject nextSegment)
+    {
+        return Instantiate(nextSegment, GetBuildPoint(currentSegment.transform, nextSegment.transform), Quaternion.identity, transform);
+    }
+
+    private Vector3 GetBuildPoint(Transform currentSegment, Transform nextSegment)
+    {
+        return new Vector3(transform.position.x, currentSegment.position.y + currentSegment.localScale.y / 2f + nextSegment.localScale.y / 2f, transform.position.z);
+    }
 }
